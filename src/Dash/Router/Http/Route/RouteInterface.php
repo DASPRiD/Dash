46e429af5f003e2f338df2df93fcdf30056ec2a8
Ben Scholzen <mail@dasprids.de>
2013-11-21 14:20:45 +0100

    Remove MVC namespace and make the router a top level component

---
+++ src/Dash/Router/Http/Route/RouteInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Dash
+ *
+ * @link      http://github.com/DASPRiD/Dash For the canonical source repository
+ * @copyright 2013 Ben Scholzen 'DASPRiD'
+ * @license   http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+namespace Dash\Router\Http\Route;
+
+use Dash\Router\Http\RouteMatch;
+use Zend\Http\Request as HttpRequest;
+
+/**
+ * Interface every HTTP route must implement.
+ */
+interface RouteInterface
+{
+    /**
+     * Matches a request at a given path offset.
+     *
+     * @param  HttpRequest $request
+     * @param  int         $pathOffset
+     * @return null|RouteMatch
+     */
+    public function match(HttpRequest $request, $pathOffset);
+
+    public function assemble();
+}
