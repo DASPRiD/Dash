2770a514064a93f30d5cfae09118e1b0ee521cc3
Ben Scholzen <mail@dasprids.de>
2013-12-06 13:41:42 +0100

    Move Module.php into proper directory and add alias

---
+++ src/Dash/Module.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Dash
+ *
+ * @link      http://github.com/DASPRiD/Dash For the canonical source repository
+ * @copyright 2013 Ben Scholzen 'DASPRiD'
+ * @license   http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+namespace Dash;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
